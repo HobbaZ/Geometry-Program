d8a83c4fc59e6b943b4964a338c000711d42bd79
Zac Hobba <83373871+HobbaZ@users.noreply.github.com>
2021-05-15 19:59:30 +1000

    Add files via upload
    Now with input validators at input fields

--- Geometry Program.py
+++ Geometry Program.py
@@ -1,8 +1,22 @@
+
+
+import random, math, pygame
+
+#pygame setup
+#pygame.init()
+#screen = pygame.display.set_mode((650,400))
+#pygame.display.set_caption("Geometry Program")
+#screen.fill([0,0,0])
+
+#press X in game window to exit game
+#while True:
+#    for event in pygame.event.get():
+#        if event.type == pygame.QUIT:
+#            sys.exit()
+
 print("Geometry Program")
-
-import random
-import math
-
+#pygame.display.update()
+        
 class Triangle:
     def __init__(self, width, height):
         self.width = width
@@ -29,6 +43,8 @@ class Square:
         return area
     
     def getLength(self):
+        #a = Square.__init__.getArea() Can't seem to reference area
+        #height = float(a/self.length)
         height = float((self.length*self.length)/self.length)
         return height
 
@@ -54,25 +70,35 @@ class Circle:
         self.radius = radius
 
     def getArea(self):
-        area = float(3.1416*(self.radius*self.radius))
+        area = float(math.pi*(self.radius*self.radius))
         return area
     
     def getDiameter(self):
-        diameter = float((math.sqrt((3.1416*(self.radius*self.radius))/3.1416))*2)
+        diameter = float((math.sqrt((math.pi*(self.radius*self.radius))/math.pi))*2)
         return diameter
 
     def getCircumference(self):
-        circumference = float((2*3.1416)*self.radius) 
+        circumference = float((2*math.pi)*self.radius) 
         return circumference
     
     def getRadius(self):
-        radi = float (math.sqrt((3.1416*(self.radius*self.radius))/3.1416)) 
+        radi = float (math.sqrt((math.pi*(self.radius*self.radius))/math.pi)) 
         return radi 
 
 
-generateProblems = (int(input("How many math questions would you like to generate? ")))
+while True: #validate input
+    try: 
+        generateProblems = int(input("How many math questions would you like to generate? "))
+        if (generateProblems == 0):
+            print("You have to answer at least one question")
+            generateProblems = int(input("How many math questions would you like to generate? ")) 
+        break
+    except ValueError:
+        #if int not entered
+        print("Not a number, try again") 
 
-#Get random problem
+
+#Get random problem, also acts as question checker (change range to target specific question you want to check)
 def randomProblem():
     randQuestionType = random.randint(9,11)
 #triangle
@@ -113,33 +139,53 @@ def randomProblem():
 #____________Triangle functions_______________
 
 def calculateTriArea():
-    width = random.randint(1,20)
-    height = random.randint(1,20) 
-    myTriangle = Triangle(width, height)
-    print("A triangle has a width of", myTriangle.width,\
-          "and a height of", myTriangle.height)
-    triangleArea = myTriangle.getArea()
-    answer = (float(input("What is it's area?  ")))
-    if (answer == triangleArea):
-        print("Yes, ", answer, "m\u00B2 is correct")
-    else:
-        print("Incorrect, the triangle's area is:",triangleArea, "m\u00B2")
-        print("Remember, you can calculate a triangle's area by multipying width by height and dividing by 2")
+   width = random.randint(1,20)
+   height = random.randint(1,20) 
+   myTriangle = Triangle(width, height)
+   print("A triangle has a width of", myTriangle.width,\
+   "and a height of", myTriangle.height)
+   triangleArea = myTriangle.getArea()
+    
+   while True: #validate input
+    try:
+        answer = (float(input("What is it's area?  ")))
+        #if int entered and correct
+        if (answer == triangleArea):
+            print("Yes, ", answer, "m\u00B2 is correct")
+            break # #break loop to go to next question
+        else:
+            # if int entered and incorrect
+            print("Incorrect, the triangle's area is:",triangleArea, "m\u00B2")
+            print("Remember, you can calculate a triangle's area by multipying width by height and dividing by 2")
+            break # #break loop to go to next question
+    except ValueError:
+        #if int not entered
+        print("Not a number, try again")
+        
        
 def calculateTriHeight():
    width = random.randint(1,20)
    height = random.randint(1,20)
    myTriangle = Triangle(width, height)
-   triangleArea = myTriangle.getArea() 
+   triangleArea = myTriangle.getArea()
    print("A triangle has a width of", myTriangle.width,\
           "and an area of", triangleArea, "m\u00B2")
    triangleHeight = myTriangle.getHeight()
-   answer = (float(input("What is it's height?  "))) 
-   if (answer == triangleHeight):
-        print("Yes, ", answer, "m is correct")
-   else:
-        print("Incorrect, the triangle's height is:",triangleHeight, "m")
-        print("Remember, you can calculate a triangle's height by multiplying area by 2 and dividing by width")
+   
+   while True: #validate input
+    try: 
+       answer = (float(input("What is it's height?  ")))
+      #if int entered and correct 
+       if (answer == triangleHeight):
+            print("Yes, ", answer, "m is correct")
+            break #break loop to go to next question
+       else:
+            print("Incorrect, the triangle's height is:",triangleHeight, "m")
+            print("Remember, you can calculate a triangle's height by multiplying area by 2 and dividing by width")
+            break #break loop to go to next question 
+    except ValueError:
+        #if int not entered
+        print("Not a number, try again") 
 
 def calculateTriWidth():
    width = random.randint(1,20)
@@ -149,12 +195,20 @@ def calculateTriWidth():
    print("A triangle has a height of", myTriangle.height,\
           "and an area of", triangleArea, "m\u00B2")
    triangleWidth = myTriangle.getWidth()
-   answer = (float(input("What is it's width?  "))) 
-   if (answer == triangleWidth):
-        print("Yes, ", answer, "m is correct")
-   else:
-        print("Incorrect, the triangle's width is:",triangleWidth, "m")
-        print("Remember, you can calculate a triangle's width by multiplying area by 2 and dividing by height")
+
+   while True: #validate input
+    try: 
+       answer = (float(input("What is it's width?  "))) 
+       if (answer == triangleWidth):
+            print("Yes, ", answer, "m is correct")
+            break
+       else:
+            print("Incorrect, the triangle's width is:",triangleWidth, "m")
+            print("Remember, you can calculate a triangle's width by multiplying area by 2 and dividing by height")
+            break
+    except ValueError:
+        #if int not entered
+        print("Not a number, try again") 
 
 #____________Square functions_______________
 
@@ -163,25 +217,39 @@ def calculateSqrArea():
     mySquare = Square(length)
     print("A square has a width and a height of", mySquare.length)
     squareArea = mySquare.getArea()
-    answer = (float(input("What is it's area?  ")))
-    if (answer == squareArea):
-        print("Yes, ", answer, "m\u00B2 is correct")
-    else:
-        print("Incorrect, the square's area is:",squareArea, "m\u00B2")
-        print("Remember, you can calculate a square's area by multipying width by height")
+    while True: #validate input
+     try: 
+        answer = (float(input("What is it's area?  ")))
+        if (answer == squareArea):
+            print("Yes, ", answer, "m\u00B2 is correct")
+            break
+        else:
+            print("Incorrect, the square's area is:",squareArea, "m\u00B2")
+            print("Remember, you can calculate a square's area by multipying width by height")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again") 
 
 def calculateSqrLength():
     length = random.randint(1,20) 
     mySquare = Square(length)
-    squareArea = mySquare.getArea() 
+    squareArea = mySquare.getArea()
     print("A square has an area of", squareArea,"m\u00B2 and a length of", mySquare.length)
     squareLength = mySquare.getLength()
-    answer = (float(input("What is the length of the opposite side?  ")))
-    if (answer == squareLength):
-        print("Yes, ", answer, "m is correct")
-    else:
-        print("Incorrect, the square's opposite length is:",squareLength, "m")
-        print("Remember, a square's opposite length is the same as all sides are equal length")
+ 
+    while True: #validate input
+     try:  
+        answer = (float(input("What is the length of the perpendicular side?  ")))
+        if (answer == squareLength):
+            print("Yes, ", answer, "m is correct")
+            break
+        else:
+            print("Incorrect, the square's opposite length is:",squareLength, "m")
+            print("Remember, a square's opposite length is the same as all sides are equal length")
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again") 
 
 #____________Rectangle functions_______________
 
@@ -192,12 +260,20 @@ def calculateRectArea():
     print("A rectangle has a width of", myRectangle.width,\
           "and a height of", myRectangle.height)
     rectangleArea = myRectangle.getArea()
-    answer = (float(input("What is it's area?  ")))
-    if (answer == rectangleArea):
-        print("Yes, ", answer, "m\u00B2 is correct")
-    else:
-        print("Incorrect, the rectangle's area is:",rectangleArea, "m\u00B2")
-        print("Remember, you can calculate a rectangle's area by multipying width by height")
+    while True: #validate input
+     try: 
+        answer = (float(input("What is it's area?  ")))
+        if (answer == rectangleArea):
+            print("Yes, ", answer, "m\u00B2 is correct")
+            break
+        else:
+            print("Incorrect, the rectangle's area is:",rectangleArea, "m\u00B2")
+            print("Remember, you can calculate a rectangle's area by multipying width by height")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again")
+    
        
 def calculateRectHeight():
    width = random.randint(1,20)
@@ -207,12 +283,19 @@ def calculateRectHeight():
    print("A rectangle has a width of", myRectangle.width,\
           "and an area of", rectangleArea, "m\u00B2")
    rectangleHeight = myRectangle.getHeight()
-   answer = (float(input("What is it's height?  "))) 
-   if (answer == rectangleHeight):
-        print("Yes, ", answer, "m is correct")
-   else:
-        print("Incorrect, the rectangle's height is:",rectangleHeight, "m")
-        print("Remember, you can calculate a rectangle's height by dividing the area by the width")
+   answer = (float(input("What is it's height?  ")))
+   while True: #validate input
+     try:
+       if (answer == rectangleHeight):
+            print("Yes, ", answer, "m is correct")
+            break
+       else:
+            print("Incorrect, the rectangle's height is:",rectangleHeight, "m")
+            print("Remember, you can calculate a rectangle's height by dividing the area by the width")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again")
 
 def calculateRectWidth():
    width = random.randint(1,20)
@@ -222,12 +305,19 @@ def calculateRectWidth():
    print("A rectangle has a width of", myRectangle.height,\
           "and an area of", rectangleArea, "m\u00B2")
    rectangleWidth = myRectangle.getWidth()
-   answer = (float(input("What is it's height?  "))) 
-   if (answer == rectangleWidth):
-        print("Yes, ", answer, "m is correct")
-   else:
-        print("Incorrect, the rectangle's width is:",rectangleWidth, "m")
-        print("Remember, you can calculate a rectangle's height by dividing the area by the height")
+   answer = (float(input("What is it's height?  ")))
+   while True: #validate input
+     try: 
+       if (answer == rectangleWidth):
+            print("Yes, ", answer, "m is correct")
+            break
+       else:
+            print("Incorrect, the rectangle's width is:",rectangleWidth, "m")
+            print("Remember, you can calculate a rectangle's width by dividing the area by the height")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again")
 
 #____________Circle functions_______________
         
@@ -237,37 +327,58 @@ def calculateCircArea():
     print("A circle has a radius of", myCircle.radius)
     circleArea = myCircle.getArea()
     answer = (float(input("What is it's area?  ")))
-    if (answer == circleArea):
-        print("Yes, ", answer, "m\u00B2 is correct")
-    else:
-        print("Incorrect, the circle's area is:",circleArea, "m\u00B2")
-        print("Remember, you can calculate a circle's area by multipying \u03C0 by radius\u00B2")
+    while True: #validate input
+     try:
+        if (answer == circleArea):
+            print("Yes, ", answer, "m\u00B2 is correct")
+            break
+        else:
+            print("Incorrect, the circle's area is:",(format(circleArea, "0.1f")), "m\u00B2")
+            print("Remember, you can calculate a circle's area by multipying \u03C0 by radius\u00B2")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again")
 
 def calculateCircDiameter():
     radius = random.randint(1,20) 
     myCircle = Circle(radius)
     circleArea = myCircle.getArea() 
-    print("A circle has an area of", circleArea,"m\u00B2")
+    print("A circle has an area of", (format(circleArea, "0.1f")),"m\u00B2")
     circleDiameter = myCircle.getDiameter()
     answer = (float(input("What is it's diameter?  ")))
-    if (answer == circleDiameter):
-        print("Yes, ", answer, "m is correct")
-    else:
-        print("Incorrect, the circle's diameter is:",circleDiameter, "m")
-        print("Remember, you can calculate a circle's diameter by dividing the area by \u03C0 and multiplying by 2")
+    while True: #validate input
+     try:
+        if (answer == circleDiameter):
+            print("Yes, ", answer, "m\u00B2 is correct")
+            break
+        else:
+            print("Incorrect, the circle's diameter is:",(format(circleDiameter, "0.1f")), "m")
+            print("Remember, you can calculate a circle's diameter by multipying radius by 2")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again")
 
 def calculateCircRadius():
     radius = random.randint(1,20) 
     myCircle = Circle(radius)
     circleArea = myCircle.getArea() 
-    print("A circle has an area of", circleArea,"m\u00B2")
+    print("A circle has an area of", (format(circleArea, "0.1f")),"m\u00B2")
     circleRadius = myCircle.getRadius() 
     answer = (float(input("What is it's radius?  ")))
-    if (answer == circleRadius):
-        print("Yes, ", answer, "m is correct")
-    else:
-        print("Incorrect, the circle's radius is:",circleRadius, "m")
-        print("Remember, you can calculate a circle's radius by finding the square root of dividing the area by \u03C0")
+    while True: #validate input
+     try:
+        if (answer == circleRadius):
+            print("Yes, ", answer, "m\u00B2 is correct")
+            break
+        else:
+            print("Incorrect, the circle's radius is:",(format(circleRadius, "0.1f")), "m")
+            print("Remember, you can calculate a circle's radius by finding the square root of dividing the area by \u03C0")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again")
 
 def calculateCircCircumference():
     radius = random.randint(1,20) 
@@ -276,15 +387,23 @@ def calculateCircCircumference():
     print("A circle has an radius of", myCircle.radius)
     circleCircumference = myCircle.getCircumference() 
     answer = (float(input("What is it's circumference?  ")))
-    if (answer == circleCircumference):
-        print("Yes, ", answer, "m is correct")
-    else:
-        print("Incorrect, the circle's radius is:",circleCircumference, "m")
-        print("Remember, you can calculate a circle's circumference by multiplying the radius by \u03C0 by 2")
+    while True: #validate input
+     try:
+        if (answer == circleCircumference):
+            print("Yes, ", answer, "m\u00B2 is correct")
+            break
+        else:
+            print("Incorrect, the circle's circumference is:",(format(circleCircumference, "0.1f")), "m")
+            print("Remember, you can calculate a circle's circumference by multiplying the radius by \u03C0 by 2")
+            break
+     except ValueError:
+        #if int not entered
+        print("Not a number, try again")
+
 
 # inputs (now changed to local inputs for questions, global inputs give variations of same ints)
 #width = random.randint(1,20)
-#height = random.randint(1,20) 
+#height = random.randint(1,20)
 
 for i in range (1, (generateProblems+1)):
      print ("") 
